3f43266cbd3374671baa6973acdc9760e14e7391
werecapy <koempel.elizabeth@gmail.com>
2025-03-13 16:39:05 -0400

    Update google-doc-url.js
    Added the link to the sheets and the api key

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Zr-wd_ceRKnNm3Wt7MRewVr02-N6Cp7GUcEq9r6OvmQ/edit?gid=164271551#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCN6NK28Bt1BysfJl_x5RuBrZWq_n9JRCA';
